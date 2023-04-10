e14940d4a479ef3c18a6ca6261debd0b85c78ab2
BarsChronicle <rzhang88@binghamton.edu>
2023-04-09 23:50:08 -0400

    raspberry pi code

---
+++ RaspPi_Programs/compute_export_enf.py
@@ -0,0 +1,211 @@
+from googleapiclient.http import MediaFileUpload
+from Google import Create_Service
+from datetime import datetime
+import pyenf
+import librosa
+import csv
+import numpy as np
+import time
+import os
+import pandas as pd
+import shutil
+
+# Constants for file locations
+folder_archive = 'Archived_Recordings/'
+folder_audio = 'Power_Recordings/'
+folder_enf = 'ENF_Data/'
+
+## Create google drive service instance
+CLIENT_SECRET_FILE = 'credentials.json'
+API_NAME = 'drive'
+API_VERSION = 'v3'
+SCOPES = ['https://www.googleapis.com/auth/drive']
+folder_database_id = '1ZAFMQeStQJEgWT4JEiVeSRIvZOwHNxf5' #upload to database folder
+dev = 'Dev1'
+
+# Create Google Drive service instance
+service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+def compute_ENF(filepath, duration):
+    #parameters for the STFT algorithm
+
+    fs = 1000  # downsampling frequency
+    nfft = 8192
+    frame_size = 2  # change it to 6 for videos with large length recording
+    overlap = 0
+
+    ## Reading from csv file - takes tooo long
+    #enf_signal = read_enf_signal(filepath)
+
+    ## Loading audio file
+    enf_signal_filename = filepath
+    enf_signal, fs = librosa.load(enf_signal_filename, sr=fs)  # loading the power ENF data
+
+    enf_signal_object = pyenf.pyENF(signal0=enf_signal, fs=fs, nominal=60, harmonic_multiples=1, duration=0.1,
+                                    strip_index=0, frame_size_secs=frame_size, nfft=nfft, overlap_amount_secs=overlap)
+    enf_spectro_strip, enf_frequency_support = enf_signal_object.compute_spectrogam_strips()
+    enf_weights = enf_signal_object.compute_combining_weights_from_harmonics()
+    enf_OurStripCell, enf_initial_frequency = enf_signal_object.compute_combined_spectrum(enf_spectro_strip,
+                                                                                          enf_weights,
+                                                                                          enf_frequency_support)
+    estimated_ENF = enf_signal_object.compute_ENF_from_combined_strip(enf_OurStripCell, enf_initial_frequency)
+    estimated_ENF = np.array(estimated_ENF).T.flatten()
+    estimated_ENF = estimated_ENF[:(duration//frame_size)] # exclude extra ENF point
+    return estimated_ENF
+
+def write_data(csv_filename, data): # write data to csv file
+    cnt = 0
+    timestamp = parse_filename(csv_filename)
+    UTC_timestamp = f"UTC: {timestamp[0]}-{timestamp[1]}-{timestamp[2]} {timestamp[3]}:{timestamp[4]}:{timestamp[5]}"
+    
+    with open(folder_enf + dev + '_ENF_Hr' + str(timestamp[3]) + '.csv', 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+
+        csv_writer.writerow([UTC_timestamp, ' Duration: 1 Hour'])
+        for item in data: # Counter, ENF value
+            csv_writer.writerow([str(cnt), str(item)])
+            cnt += 1
+
+def read_file(filepath): # return date read from csv file
+    with open(filepath, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+
+        UTC_date = (list(csv_reader))[0][0]
+        
+    return f'{UTC_date[5:9]}_{UTC_date[10:12]}_{UTC_date[13:15]}'
+
+def parse_filename(filename): # %Y_m_d_H_M_S
+    
+    timestamp = []
+    for i in range(6):
+        timestamp.append(filename[0:filename.find('_')])
+        filename = filename[filename.find('_')+1:]
+     
+    return timestamp
+
+def create_folder(drive_id, folder_name):
+    file_metadata = {
+        'name' : folder_name,
+        'mimeType' : 'application/vnd.google-apps.folder',
+        'parents' : [drive_id] #create folder under another folder
+    }
+
+    file = service.files().create(
+        body=file_metadata,
+        fields='id',
+        supportsAllDrives=True #allow access to share drive
+    ).execute()
+
+    folder_id = file.get('id')
+    return folder_id
+
+def upload_file(folder_id, file_name):
+    mime_types = 'text/csv' # mimetype for each file type
+
+    file_metadata = { #goes to body
+        'name' : file_name,
+        'parents' : [folder_id]
+    }
+
+    # specify folder name and provide mimetype; store output to object called media
+    media = MediaFileUpload(folder_enf+'{0}'.format(file_name), mimetype=mime_types)
+
+    service.files().create(
+        body = file_metadata,
+        media_body = media,
+        fields = 'id',
+        supportsAllDrives=True #allow access to share drive
+    ).execute()
+
+def scan_folders(target, file_name):
+    query = f"parents = '{folder_database_id}'" # folder (in URL) to upload
+
+    while(True):
+        try:
+            response = service.files().list(q=query, includeItemsFromAllDrives=True, supportsAllDrives=True).execute()
+        except Exception:
+            pass
+        else:
+            files = response.get('files')
+            break
+
+    df = pd.DataFrame(files)
+    df_id = df.get("id")
+    df_name = df.get("name")
+
+    create_folder_flag = 0
+    #grab id and name
+    try:
+        for id, name in zip(df_id,df_name):
+            # check if folder_name exists
+            if target in str(name): # yes  ->> upload file
+                upload_file(id, file_name)
+                create_folder_flag = 1
+                break
+    except:
+        pass
+           
+    if create_folder_flag == 0:    # no ->> create folder
+        new_folder_id = create_folder(folder_database_id, dev + '_Power_ENF_'+target)
+        upload_file(new_folder_id, file_name)
+
+def remove_file(filepath):
+    try:
+        os.remove(filepath) #remove file
+    except FileNotFoundError:
+        print('File not found')
+    except PermissionError:
+        print('You do not have permission to delete')
+
+def cleanup():
+    list_archived_files = os.listdir(folder_archive)
+    if (not list_archived_files):
+        return #exit function
+    for filename in list_archived_files:
+        filepath = folder_archive + filename
+        c_time = os.path.getctime(filepath) # File creation timestamp
+
+        dt_c = datetime.now() # Current local timestamp
+        delta_time = dt_c - datetime.fromtimestamp(c_time)
+
+        if (delta_time.days > 14): # Remove files older than 2 weeks
+            remove_file(filepath)
+
+def main():
+    dur_minute = 15
+    duration = (60*dur_minute) 
+
+    while(True):
+        cleanup() # Archived Recording maintenance
+
+        # loop back to check for more files to estimate
+        list_audio_files = os.listdir(folder_audio)
+
+        if (not list_audio_files): 
+            print('Folder empty')
+            time.sleep(1800) # sleep for some time
+            continue
+        
+        for filename in list_audio_files: # compute ENF in every file
+            filepath = folder_audio + filename
+            ENF = compute_ENF(filepath, duration)
+            write_data(filename[:-3], ENF) # write ENF to csv
+        
+        # export to Google Drive (check year,month,day)
+        list_ENF_files = os.listdir(folder_enf)
+        
+        for filename in list_ENF_files:
+            filepath = folder_enf + filename
+            target = read_file(filepath)# Check date from csv file
+            scan_folders(target,filename) # scan and upload files to proper folder
+        
+        for filename in list_audio_files: # Archive audio recordings
+            filepath = folder_audio + filename
+            shutil.move(filepath, folder_archive)
+
+        for filename in list_ENF_files: # All estimation complete and exported, clean up files in ENF_Data
+            filepath = folder_enf + filename
+            remove_file(filepath) 
+
+if __name__ == '__main__':
+    main()
---
+++ RaspPi_Programs/power_rec_even_hr.py
@@ -0,0 +1,79 @@
+import ntplib
+import datetime
+import time
+import sounddevice as sd
+import soundfile as sf
+import sys
+import os
+
+folder_audio = "Power_Recordings/"
+
+def power_rec(fs, duration, path): #buffer audio
+    print(f"Enter recording: {duration} seconds")
+    buf_recording = sd.rec(int(duration * fs), samplerate=fs, channels=1) # buffer for the duration
+    sd.wait()
+
+    sf.write(folder_audio+path+".wav", data=buf_recording, samplerate=fs)
+    print("Exit recording")
+
+def restart():
+    print("argv: ", sys.argv)
+    print("sys executable: ", sys.executable)
+    print("Restart now!")
+    os.execv(sys.executable, ['python'] + sys.argv)
+    
+def main():
+    #parameters for the STFT algorithm
+    dur_minute = 60
+    duration = (60*dur_minute) + 10
+    fs = 1000
+
+    ntpc = ntplib.NTPClient()
+    host = 'pool.ntp.org'
+    #host = '1.us.pool.ntp.org'
+    UTC_ref = time.time()
+    
+    # Synchronize with NTP
+    try:
+        UTC_ref = ntpc.request(host).tx_time
+
+    except ntplib.NTPException:
+        restart()
+    else:
+        UTC_timestamp = datetime.datetime.utcfromtimestamp(UTC_ref)
+        #print(f'Synchronize at: {UTC_timestamp}')
+    
+    time_until_record = 60 - UTC_timestamp.minute
+    time_offset = 3
+    even = 0
+
+    if (UTC_timestamp.hour % 2 == even): # Next hour is odd, sleep over the odd hour block
+        time_until_record += 60
+    # Sleep some time, don't constantly use CPU resource for counting secs
+    if (time_until_record > time_offset): # when there are more than 3 minutes until recording session
+        print(f'Sleep {(time_until_record - time_offset)*60} secs')
+        time.sleep((time_until_record - time_offset)*60)
+        restart()
+    
+    start_time = time.time()
+
+    ## Offset to synchronized timestamp
+    while(True): 
+        end_time = time.time()
+
+        if (end_time - start_time >= .01): # count seconds to increment synchronized UTC timestamp
+            UTC_ref = UTC_ref + (time.time() - start_time)
+            start_time = time.time() #reset start time
+            UTC_timestamp = datetime.datetime.utcfromtimestamp(UTC_ref)
+            print(f'Sec: {UTC_timestamp.second} ->>>>>>>>> {UTC_timestamp}')
+        
+        if (UTC_timestamp.hour % 2 == even and UTC_timestamp.minute == 0 and UTC_timestamp.second == 0): # Fresh hour, start recording
+            path = UTC_timestamp.strftime('%Y_%m_%d_%H_%M_%S')
+            print(f'Recording Time: {UTC_timestamp}')
+            power_rec(fs,duration, path)
+            break
+
+    restart()         
+
+if __name__ == '__main__':
+    main()
